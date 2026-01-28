644ff83178d13cae09bd74c97dad8c2c167b39c8
bcvance <ben@vancenet.net>
2026-01-28 15:05:40 -0600

    first commit

---
+++ 1:28 R Practice.R
@@ -0,0 +1,72 @@
+setwd('/Users/b.vance/Library/CloudStorage/OneDrive-UniversityofKansas/Research Methods/R Files/R Drafts')
+
+a <- 3
+a
+a + 5
+b <- 7
+b
+b - 5
+a + b
+rm(b)
+is(a)
+class(a)
+
+fibonacci10 <- c(1, 1, 2, 3, 5, 8, 13, 21, 34, 55)
+fibonacci10
+
+vnum <- 1:5
+vnum
+
+vnum1 <- c(1:5, 10, 5)
+vnum1
+
+vnum2 <- seq(0, 100, 10)
+vnum2
+
+vnum3 <- rep(1:5, 3)
+vnum3
+
+summary(fibonacci10)
+
+sort(vnum1)
+
+sort(vnum1, decreasing=TRUE)
+
+vnum1
+
+vnumX <- sort(vnum1)
+
+vnumX
+
+length(vnum1)
+
+vnum1[c(3, 6)]
+
+vnum1[c(-1)]
+
+vnum1[vnum1 > 2]
+
+vnum1[vnum1 != 5]
+
+vnum1[vnum1 < 3 | vnum1 == 10]
+
+vnum1[vnum1 > 4 & vnum1 < 10]
+
+which(vnum1 == 5)
+
+w <- c(20, 30, 40)
+
+y <- c(vnum1, w)
+y
+
+sum(vnum1)
+
+vnum1 + 3
+
+vnum1 * 2
+
+vnum1 ^ 2
+
+log(vnum1)
+
+round(log(vnum1), 2)

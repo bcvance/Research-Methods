adbbf75a35a8fd74230c4a72907b9435ca0b3747
bcvance <ben@vancenet.net>
2026-02-02 15:17:12 -0600

    additional practice 2/2

--- R Practice/2:2 R Practice.R
+++ R Practice/2:2 R Practice.R
@@ -1,7 +1,6 @@
 data <- read.csv("720-data.csv")
 
 str(data)
-summary(df)
 head(df)
 dim(data)
 
@@ -16,7 +15,6 @@ head(data)
 
 levels(data$Related)
 levels(data$Related) <- c("rel", "unrel")
-levels(data$Related)
 head(data)
 
 data.L2 <- data[data$Group == "L2", ]
@@ -25,3 +23,19 @@ data.L2 <- data.L2[order(data.L2$Item, data.L2$Subject), ]
 head(data.L2)
 
 write.csv(data.L2, "L2-data.csv")
+
+#Additional Practice
+
+summary(data$RT)
+
+data <- data[data$RT > 299 & data$RT < 2001, ]
+summary(data)
+
+hist(data$RT)
+
+data$LogRT <- log(data$RT)
+head(data)
+hist(data$LogRT)
+
+agRT <- aggregate(data = data, RT ~ Condition, FUN = mean)
+agRT
